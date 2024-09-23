60aa82a6d4c911743f130cc9f95505ca585972db
Inna Saienko <inna.saienko.nl@gmail.com>
2024-09-23 09:00:40 +0200

    Create carousel banner component.

---
+++ src/components/CarouselBanner/CarouselBanner.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./CarouselBanner.scss";
+
+export default function CarouselBanner(props) {
+  const { data = [] } = props;
+  const navigate = useNavigate();
+  const topFive = data.slice(0, 5);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: false,
+
+    // autoplaySpeed: 5000,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  const handleClick = (product) => {
+    navigate(`/product/${product.id}`, { state: { product } });
+  };
+
+  return (
+    <Slider {...settings}>
+      {topFive.map((item) => {
+        console.log(item);
+        return (
+          <div
+            className="slide-item"
+            key={item.id}
+            onClick={() => handleClick(item)}
+          >
+            <div className="slide__content">
+              <div className="slide__left">
+                <div className="slide__label hit">
+                  {item.brand ? item.brand.toUpperCase() : ""}
+                </div>
+                <button className="button">Buy Now</button>
+                <div className="slide__product-info">
+                  <h2 className="title-2">{item.name}</h2>
+                </div>
+              </div>
+              <div className="slide__right">
+                <Link className="slide-link" to="#one!">
+                  <img src={item.image_link} alt="Product image" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </Slider>
+  );
+}
