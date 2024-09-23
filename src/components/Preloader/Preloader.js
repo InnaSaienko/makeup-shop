4cbf8659571d384b09b6ed85b10d6a56ee4b940a
Inna Saienko <inna.saienko.nl@gmail.com>
2024-09-23 09:01:09 +0200

    Create Preloader component.

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,11 @@
+import "../Preloader/Preloader.scss";
+
+function Preloader() {
+    return (
+        <div className="progress">
+            <div className="indeterminate"></div>
+        </div>
+    );
+}
+
+export { Preloader };
