1f378ec1254291f667e37e9fe7e73ccde3e106ac
Inna Saienko <inna.saienko.nl@gmail.com>
2025-01-11 14:02:04 +0100

    Changed footer  to own styles

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,32 +1,61 @@
 import { NavLink, Link } from "react-router-dom";
+import "./Footer.scss"
 
 function Footer() {
   return (
-    <footer className="page-footer">
-    <div className="container">
-      <div className="row">
-        <div className="col l6 s12">
-          <h5 className="white-text">Footer Content</h5>
-          <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-        </div>
-        <div className="col l4 offset-l2 s12">
-          <h5 className="white-text">Links</h5>
-          <ul>
-            <li><NavLink className="grey-text text-lighten-3" to="#!">Link 1</NavLink></li>
-            <li><NavLink className="grey-text text-lighten-3" to="#!">Link 2</NavLink></li>
-            <li><NavLink className="grey-text text-lighten-3" to="#!">Link 3</NavLink></li>
-            <li><NavLink className="grey-text text-lighten-3" to="#!">Link 4</NavLink></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div className="footer-copyright">
+    <footer className="footer">
       <div className="container">
-      © {new Date().getFullYear()} Copyright Text
-      <Link className="grey-text text-lighten-4 right" to="#!">More Links</Link>
+        <div className="row">
+          <div className="footer-contacts">
+            <div className="cols">
+              <div className="col">
+                <h3>
+                  <NavLink to="/">MakeUp</NavLink>
+                </h3>
+                <ul>
+                  <NavLink to="#!">Who is MakeUp</NavLink>
+                  <NavLink to="#!">MakeUP App</NavLink>
+                  <NavLink to="#!">Promotional gifts</NavLink>
+                </ul>
+              </div>
+              <div className="col">
+                <h3>
+                  <NavLink to="/">Customer service</NavLink>
+                </h3>
+                <ul>
+                  <NavLink to="#!">About delivery</NavLink>
+                  <NavLink to="#!">Discover our Gifts Cards</NavLink>
+                  <NavLink to="#!">Glossary</NavLink>
+                  <NavLink to="#!">Questions & Answers</NavLink>
+                </ul>
+              </div>
+              <div className="col">
+                <h3>
+                  <NavLink to="/">Service</NavLink>
+                </h3>
+                <ul>
+                  <NavLink to="#!">Bonuses</NavLink>
+                  <NavLink to="#!">Gift certificate</NavLink>
+                  <NavLink to="#!">MakeUp return</NavLink>
+                </ul>
+              </div>
+            </div>            
+          </div>
+          <div className="footer-nav">
+            <div className="footer-nav_phones"></div>
+            <div className="footer-nav_worktime"></div>
+            <div className="footer-nav_callback"></div>
+            <div className="cooperation"></div>
+          </div>
+        </div>
       </div>
-    </div>
-  </footer>
+      <div className="footer-copyright">
+        <div className="container">
+          © {new Date().getFullYear()} Copyright Text
+          <Link className="grey-text text-lighten-4 right" to="#!">More Links</Link>
+        </div>
+      </div>
+    </footer>
   );
 }
 
