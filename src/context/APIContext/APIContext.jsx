fbb81d1f32eaedcba60152da52b2ce2f0deeeb60
Inna Saienko <inna.saienko.nl@gmail.com>
2025-01-16 16:29:58 +0100

    Imported video from assets

--- src/context/APIContext/APIContext.jsx
+++ src/context/APIContext/APIContext.jsx
@@ -5,22 +5,33 @@ const APIContext = createContext({});
 export function useAPIContext() {
   return useContext(APIContext);
 }
+const importVideos = () => {
+  const context = require.context('../../assets/videos', false, /\.(mp4|webm)$/);
+  console.log("Video keys:", context.keys());
+  return context.keys().map((key) => ({
+      src: context(key),
+      title: key.replace('./', '').replace(/\.(mp4|webm)$/, ''),
+  }));
+};
 
 export function APIProvider({ children }) {
   const [isDownloaded, setIsDownloaded] = useState(false);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [videos, setVideos] = useState([]);
 
   const topFive = data.slice(0, 5);
   const hit20 = data.slice(5, 21);
 
   useEffect(() => {
+    setVideos(importVideos());
     fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?rating_greater_than=4.99`)
       .then((response) => response.json())
       .then((data) => {
         setData(data);
         setLoading(false);
         setIsDownloaded(true);
+        console.log("Videos loaded:", importVideos());
       })
       .catch((error) => {
         console.error("Failed to fetch data:", error);
@@ -31,6 +42,7 @@ export function APIProvider({ children }) {
   return (
     <APIContext.Provider
       value={{
+        videos,
         topFive,
         hit20,
         loading,
