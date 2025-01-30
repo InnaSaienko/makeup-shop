286f0a37b4829f4d99f8d4a29478c615b94ab6f4
Inna Saienko <inna.saienko.nl@gmail.com>
2025-01-30 11:10:25 +0100

    replaced ProductContext to hook FetchData

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import useFetchData from "../../hooks/useFetchData.js";
 import { useBasket } from "../../context/BasketContext/BasketContext";
+import { Preloader } from "../Preloader/Preloader"
 import "./ProductDetails.scss";
 
-function ProductDetails() {
-  const location = useLocation();
-  const { product } = location.state;
-  const [selectedColor, setSelectedColor] = useState(product.product_colors[0]);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+const ProductDetails = () => {
+  const { id, subcategory } = useParams();
+  const { data, loading, error } = useFetchData({ subcategory });
   const { addProduct } = useBasket();
- 
-  if (!product) {
-    return <div>Product not found</div>;
-  }
+  const product = data.find((item) => item.id === Number(id));
+  const [selectedColor, setSelectedColor] = useState(null);
+  //   product.product_colors.length > 0 ? product.product_colors[0] : null
+  // );
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+
+  if (loading) { return <Preloader />; };
+  if (error) return <p>Error: {error}</p>;
 
   const handleColorSelect = (color) => {
-    setSelectedColor(color);
-    setIsDropdownOpen(false); // Close dropdown after selecting a color
+    setSelectedColor(color); 
+    setIsDropdownOpen(false);
   };
 
   const renderStars = (rating) => {
@@ -33,7 +38,7 @@ function ProductDetails() {
 
     return Array.from({ length: 5 }, (_, index) => (
       <div
-        key={index}
+        // key={index}
         className={`star-list__item ${index < rating ? "" : "no-rating"}`}
       >
         ★
@@ -41,17 +46,42 @@ function ProductDetails() {
     ));
   };
 
-  const variant = (color, idOfProduct) => (
-    <div className="variant" data-id={idOfProduct} onClick={() => handleColorSelect(color)}>
-      <div
-        className="variant-image"
-        style={{ backgroundColor: color.hex_value }}
-      ></div>
-      <span>{color.colour_name}</span>
-    </div>
-  );
+  const variantsOfColors =
+    product.product_colors.length > 0 ? (
+      <div className="select">
+        <label
+          className="select-product-variant"
+          data-id={product.id}
+          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+        >
+          <div className="variant">
+            <div
+              className="variant-image"
+              style={{
+                backgroundColor: product.product_colors[0].hex_value ? product.product_colors[0].hex_value : "#ccc",
+              }}
+            ></div>
+            <span>{product.product_colors[0].colour_name}</span>
+          </div>
+          <div className="dropdown-icon"></div>
+        </label>
 
-  return (
+        {isDropdownOpen && (
+          <div className="variants">
+            {product.product_colors.map((color) => (
+              <div key={color.hex_value} onClick={() => handleColorSelect(color)}>
+                <div className="variant-image" style={{ backgroundColor: color.hex_value }}></div>
+                <span>{color.colour_name}</span>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    ) : (
+      <p>No colors available</p>
+    );
+    
+    return (
     <div className="product-item">
       <div id="product-image" className="product-item__image">
         <img
@@ -72,26 +102,11 @@ function ProductDetails() {
           {product.price} {product.currency}
         </div>
       </div>
-      <div className="select">
-        <label
-          className="select-product-variant" data-id={product.id}
-          onClick={() => setIsDropdownOpen(!isDropdownOpen)} // Toggle dropdown
-        >
-          {variant(selectedColor, product.id)}
-          <div className="dropdown-icon"></div>
-        </label>
-
-        {isDropdownOpen && (
-          <div className="variants">
-            {product.product_colors.map((color, index) => (
-              <div key={index}>{variant(color)}</div>
-            ))}
-          </div>
-        )}
-      </div>
+      {variantsOfColors}
       <div className="product-item__button">
         <button className="button buy"
-        onClick={() => addProduct(product.id, selectedColor.hex_value, product.product_type)}>Buy</button>
+          onClick={() => addProduct(product.id, selectedColor?.hex_value || "", product.product_type)}
+          disabled={!selectedColor}>Buy</button>
       </div>
     </div>
   );
