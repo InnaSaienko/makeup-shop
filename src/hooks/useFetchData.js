622d2bb9536c7ba5e37761dbd041dd24deb708ae
Inna Saienko <inna.saienko.nl@gmail.com>
2025-03-11 09:58:17 +0100

    adjust state data

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 const useFetchData = (Path, filters = {}) => {
-    const [data, setData] = useState(null);
+    const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
