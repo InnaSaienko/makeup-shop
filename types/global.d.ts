51c544a2e7c98a44fb6263375a30710ac4d1018a
Inna Saienko <inna.saienko.nl@gmail.com>
2025-03-13 20:31:21 +0100

    added interface for BasketContext

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,9 +1,47 @@
 interface Product {
-    id: number;
+    id: string;
     name: string;
     api_featured_image: string;
     product_type: string;
+    category: string | null;
     price: string;
     currency: string;
     product_colors?: { hex_value: string; colour_name?: string }[];
 }
+
+interface BasketItem {
+    id: string;
+    selectedColor: string;
+    product_type: string;
+    quantity: number;
+}
+
+interface BasketContextType {
+    basketProductsContext: BasketItem[];
+    addProduct: (id: string, selectedColor: string, product_type: string) => void;
+    decreaseQuantity: (id: string, selectedColor: string) => void;
+    removeFromBasket: (id: string, selectedColor: string) => void;
+    getUniqueProductQuantity: (id: string, selectedColor: string) => number;
+    getTotalQuantity: () => number | null;
+    openBasket: () => void;
+    closeBasket: () => void;
+}
+
+interface BasketProviderProps {
+    children: ReactNode;
+}
+
+interface BasketItem extends Product {
+    selectedColor: string;
+    quantity: number;
+}
+
+interface BasketProps {
+    isOpen: boolean;
+}
+
+declare module '*.svg' {
+    const content: string;
+    export default content;
+}
+
