aa7bab29498ea597094799273abe27c64859817a
Inna Saienko <inna.saienko.nl@gmail.com>
2025-03-13 08:35:45 +0100

    add global interface Product

---
+++ types/global.d.ts
@@ -0,0 +1,9 @@
+interface Product {
+    id: number;
+    name: string;
+    api_featured_image: string;
+    product_type: string;
+    price: string;
+    currency: string;
+    product_colors?: { hex_value: string; colour_name?: string }[];
+}
