93fde0b3c6f119c6a0f0c9b5ce50beeb7257c502
Inna Saienko <inna.saienko.nl@gmail.com>
2025-03-26 11:05:39 +0100

    formatted

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,54 +1,53 @@
 import React from "react";
 import {Link, useParams} from "react-router-dom";
-import { getSubcategoryDeal } from "../../utils/getSubcategoryDeal";
+import {getSubcategoryDeal} from "../../utils/getSubcategoryDeal";
 import "./ProductCard.scss";
 
-const ProductCard = (props: Product) =>  {
-  const {id, brand, rating, name, api_featured_image: image_link } = props;
+const ProductCard = (props: Product) => {
+    const {id, brand, rating, name, api_featured_image: image_link} = props;
 
-  const { subcategory = "" } = useParams();
+    const {subcategory = ""} = useParams();
 
     const isDealData = getSubcategoryDeal(subcategory);
     const isDeal = isDealData?.deal ?? false;
 
+    return (
+        <Link className="catalog-grid__cell card" id={id} to={`/products/${id}`}>
+            <div className="card__badge">
+                {isDeal ? (<div className="card_label">DEAL</div>) : (<div className="card_label">HIT</div>)}
+            </div>
+            <div className="card-icons-wrapper">
+                <div className="to-favorite">
+                    <div
+                        data-id={id}
+                        className="to-quick-buy"
+                        data-text-add="to__selected"
+                        data-text-remove="from__selected"
+                        data-is-favorite="0"
+                    ></div>
+                </div>
+            </div>
 
-  return (
-    <Link className="catalog-grid__cell card" id={id} to={`/products/${id}`}>
-      <div className="card__badge">
-        {isDeal ? (<div className="card_label">DEAL</div>) : (<div className="card_label">HIT</div>)}
-      </div>
-      <div className="card-icons-wrapper">
-        <div className="to-favorite">
-        <div
-          data-id={id}
-          className="to-quick-buy"
-          data-text-add="to__selected"
-          data-text-remove="from__selected"
-          data-is-favorite="0"
-        ></div>
-      </div>
-      </div>
-      
-      <div className="card__image">
-        <img
-          className="card__image__link"
-          alt="image_product"
-          src={image_link}
-          loading="lazy"
-        />
-      </div>
-      <div className="card__content">
+            <div className="card__image">
+                <img
+                    className="card__image__link"
+                    alt="image_product"
+                    src={image_link}
+                    loading="lazy"
+                />
+            </div>
+            <div className="card__content">
         <span className="card__title">
           {brand}
-          <i className="material-icons right"></i>
+            <i className="material-icons right"></i>
         </span>
-        <p className="card__description__rating">
-          <span>{rating}</span>
-          <span className="card__description">{name}</span>
-        </p>
-      </div>
-    </Link>
-  );
+                <p className="card__description__rating">
+                    <span>{rating}</span>
+                    <span className="card__description">{name}</span>
+                </p>
+            </div>
+        </Link>
+    );
 }
 
-export { ProductCard };
+export {ProductCard};
