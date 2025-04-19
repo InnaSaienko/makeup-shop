51bf5e49fd58f95cb2ef7829efbaf2ebd9330b8f
Inna Saienko <inna.saienko.nl@gmail.com>
2025-04-19 08:26:13 +0200

    add floating-label styles and

--- src/components/RegisterForm/CustomInput.jsx
+++ src/components/RegisterForm/CustomInput.jsx
@@ -4,9 +4,9 @@ import "./CustomInput.scss";
 
 export const CustomInput = ({name}) => {
     const id = useId();
-    const {values, setFieldValue} = useFormikContext();
+    const { values } = useFormikContext();
     const isPasswordField = (name === 'password' || name === 'repeat_password');
-    const [showPassword, setShowPassword] = useState()
+    const [showPassword, setShowPassword] = useState();
 
     const togglePasswordVisibility = () => {
         setShowPassword((prev) => !prev);
@@ -14,11 +14,32 @@ export const CustomInput = ({name}) => {
     const formatLabel = (key) =>
         key.replace(/_/g, ' ').replace(/\b\w/g, char => char.toUpperCase());
 
+    const [isFocused, setIsFocused] = useState(false);
+
+    const hasValue = !!values[name];
+    const isActive = isFocused || hasValue;
     return (
         <div className="input-group">
-            <Field id={id} name={name} className="input" as="input"/>
-            <label htmlFor={id} className="floating-label">{formatLabel(name)}</label>
-            {isPasswordField ? (<span className="password-show-button"></span>) : ("")}
+            <Field name={name}>
+                {({field}) => (
+                    <input
+                        {...field}
+                        id={id}
+                        type={isPasswordField && !showPassword ? 'password' : 'text'}
+                        className="input"
+                        placeholder=" "
+                        onFocus={() => setIsFocused(true)}
+                        onBlur={() => setIsFocused(false)}/>
+                )}
+            </Field>
+            <label htmlFor={id} className={`floating-label ${isActive ? 'active' : ''}`}>
+                {formatLabel(name)}</label>
+            {isPasswordField ?
+                <span
+                    className={`password-button ${showPassword ? 'show' : 'hide'}`}
+                    onClick={togglePasswordVisibility}
+                /> :
+                (null)}
             <span className="bar"></span>
         </div>
     );
