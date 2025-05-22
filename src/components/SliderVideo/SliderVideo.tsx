eadee1a6c931e28b4a655baa7ec5798343cf1b9b
Inna Saienko <inna.saienko.nl@gmail.com>
2025-05-22 10:24:45 +0200

    switch SliderVideo to tsx

--- src/components/SliderVideo/SliderVideo.tsx
+++ src/components/SliderVideo/SliderVideo.tsx
@@ -1,36 +1,36 @@
 import React from 'react';
 import useVideos from "../../hooks/useFetchVideo";
-import { Preloader } from "../Preloader/Preloader";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import {Preloader} from "../Preloader/Preloader";
+import {Swiper, SwiperSlide} from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import "./SliderVideo.scss";
-import { Navigation, Pagination } from 'swiper/modules';
+import {Navigation, Pagination} from 'swiper/modules';
 import SliderTopFive from "../SliderTopFive/SliderTopFive";
 
-export const SliderVideo= () => {
-    const { videos, loading }= useVideos();
+export const SliderVideo = () => {
+    const {videos, loading} = useVideos();
 
-    if (loading) { 
-        return <Preloader />; 
+    if (loading) {
+        return <Preloader/>;
     }
 
     return (
         <div className="carouselVideo">
-            {!videos ? (<SliderTopFive />) : (
+            {!videos ? (<SliderTopFive/>) : (
                 <Swiper
                     modules={[Navigation, Pagination]}
                     spaceBetween={20}
                     slidesPerView={1}
-                    navigation={{ clickable: true }}
+                    navigation={{clickable: true}}
                     loop={true}
                 >
                     {videos.map((video, index) => (
                         <SwiperSlide key={index}>
                             <div className="slide-video">
-                                <video controls className="video"preload="metadata">
-                                    <source src={video.src} type="video/mp4" />
+                                <video controls className="video" preload="metadata">
+                                    <source src={video.src} type="video/mp4"/>
                                     Your browser does not support the video tag.
                                 </video>
                             </div>
--- src/hooks/useFetchVideo.ts
+++ src/hooks/useFetchVideo.ts
@@ -7,14 +7,14 @@ interface Video {
 
 declare var require: Require;
 
-const useVideos = (): {videos: Video[] | []; loading: boolean} => {
+const useVideos = (): { videos: Video[] | []; loading: boolean } => {
     const [videos, setVideos] = useState<Video[] | []>([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const loadVideos = async () => {
             const context = require.context('../assets/videos', false, /\.(mp4|webm)$/);
-            const videoList : Video[] = context.keys().map((key :string) => ({
+            const videoList: Video[] = context.keys().map((key: string) => ({
                 src: context(key),
                 title: key.replace('./', '').replace(/\.(mp4|webm)$/, ''),
             }));
