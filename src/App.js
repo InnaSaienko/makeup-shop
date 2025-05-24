75d7c6bf4077cf36f43b31ed70a416cfbc0b1bed
Inna Saienko <inna.saienko.nl@gmail.com>
2025-05-24 10:51:46 +0200

    apply RouterSet in App

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
 import React from "react";
 import "./App.scss";
-import { Routes, Route } from "react-router-dom";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 import { BasketProvider } from "./context/BasketContext/BasketContext";
 import { AuthorizationProvider } from "./context/AuthorizationContext/AuthorizationContext";
 import { HeaderTop } from "./layout/HeaderTop/HeaderTop";
 import { Navigation } from "./layout/Navigation/Navigation";
 import { Footer } from "./layout/Footer/Footer";
-import { Home } from "./pages/Home";
-import ProductsList from "./components/ProductsList/ProductsList";
-import ProductDetails from "./components/ProductDetails/ProductDetails";
-import {RegisterFormikForm} from "./components/RegisterForm/RegisterFormikForm";
+import RouterSet from "./components/Routes/RouterSet";
 
 function App() {
   return (
-    <>
       <div className="main-wrap">
         <ErrorBoundary>
           <AuthorizationProvider>
             <BasketProvider>
               <HeaderTop />
               <Navigation />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/products/:id" element={<ProductDetails />} />
-                <Route path="/products/:category/:subcategory" element={<ProductsList />} />
-                <Route path="/brand/:brand" element={<ProductsList />} />
-                <Route path="/register-form" element={<RegisterFormikForm />} />
-              </Routes>
+              <RouterSet />
             </BasketProvider>
           </AuthorizationProvider>
           <Footer />
         </ErrorBoundary>
       </div>
-    </>
   );
 }
 
