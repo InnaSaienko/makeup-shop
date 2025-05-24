cc5f43d32cba67b43293e675b2358094c2638640
Inna Saienko <inna.saienko.nl@gmail.com>
2025-05-24 10:51:10 +0200

    switch RouterSet to tsx

---
+++ src/components/Routes/RouterSet.tsx
@@ -0,0 +1,27 @@
+import React, {lazy, Suspense} from 'react';
+import {Route, Routes} from "react-router-dom";
+import {Preloader} from "../Preloader/Preloader";
+
+// @ts-ignore
+const Home = lazy(() => import("../../pages/Home.jsx") as Promise<{ default: React.ComponentType<any> }>);
+const ProductDetails = lazy(() => import("../../components/ProductDetails/ProductDetails") as Promise<{ default: React.ComponentType<any> }>);
+// @ts-ignore
+const RegisterFormikForm = lazy(() => import("../../components/RegisterForm/RegisterFormikForm.jsx") as Promise<{ default: React.ComponentType<any> }>);
+const ProductsList = lazy(() => import("../ProductsList/ProductsList") as Promise<{ default: React.ComponentType<any> }>);
+
+const RouterSet = () => {
+
+    return (
+        <Suspense fallback={<Preloader/>}>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/products/:id" element={<ProductDetails/>}/>
+                    <Route path="/products/:category/:subcategory" element={<ProductsList/>}/>
+                    <Route path="/brand/:brand" element={<ProductsList/>}/>
+                    <Route path="/register-form" element={<RegisterFormikForm/>}/>
+                </Routes>
+        </Suspense>
+    )
+};
+
+export default RouterSet;
