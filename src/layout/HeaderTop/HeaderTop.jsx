1cb6ebdbba8ba96cf64be8912f7f4a2e58cf2b76
Inna Saienko <inna.saienko.nl@gmail.com>
2025-05-25 10:36:09 +0200

    refactored HeaderTop structure to improve readability and maintainability

--- src/layout/HeaderTop/HeaderTop.jsx
+++ src/layout/HeaderTop/HeaderTop.jsx
@@ -1,38 +1,20 @@
 import React from "react";
-import { useBasket } from "../../context/BasketContext/BasketContext";
-import { useAuthorization } from "../../context/AuthorizationContext/AuthorizationContext";
-
-import { NavLink } from "react-router-dom";
 import "./HeaderTop.scss";
+import SearchBox from "./SearchBox";
+import LogoBox from "./LogoBox";
+import UserBox from "./UserBox";
 
 function HeaderTop() {
-  const { openBasket, getProductQuantity } = useBasket();
-  const { openAuthorization, loggedUser, guest } = useAuthorization();
 
-  return (
-    <header className="header">
-      <div className="layout">
-        <div className="header-left-row">
-          <div className="search-button"></div>
-        </div>
-
-        <div className="header-center-row">
-          <NavLink
-            to="/"
-            title="Beaty no limits"
-            className="logo"
-          />
-        </div>
-        <div className="header-right-row">
-          <button className={`authorization ${loggedUser !== guest ? "logged-in" : "logged-out"}`} 
-  onClick={openAuthorization}></button>
-          <button className={`basket-widget ${getProductQuantity() === null ? "empty" : ""}`} onClick={openBasket}>
-            <span className={`header-counter${getProductQuantity() === null ? "hidden" : ""}`}>{getProductQuantity()}</span>
-          </button>
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header className="header">
+            <div className="layout">
+                <SearchBox/>
+                <LogoBox/>
+                <UserBox/>
+            </div>
+        </header>
+    );
 }
 
-export { HeaderTop };
+export {HeaderTop};
