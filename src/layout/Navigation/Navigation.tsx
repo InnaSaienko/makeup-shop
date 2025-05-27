30ddf4cdd26f546cbd2f3faee208c0b13b3a3c1d
Inna Saienko <inna.saienko.nl@gmail.com>
2025-05-27 09:02:39 +0200

    switch Navigation to tsx

---
+++ src/layout/Navigation/Navigation.tsx
@@ -0,0 +1,35 @@
+import React, {JSX} from "react";
+import {Link} from "react-router-dom";
+import {categories} from "../../constatnts/categories";
+import "./Navigation.scss";
+
+const Navigation = (): JSX.Element => {
+    return (
+        <nav className="menu">
+            <div className="layout">
+                <ul className="menu-list">
+                    {categories.map((category, index) => (
+                        <li key={index} className="menu-list__item">
+                            <Link to="#" className="menu-list__link__category title-2"
+                                  onClick={(e) => e.preventDefault()}>
+                                {category.category}
+                            </Link>
+                            {/* Subcategories Dropdown */}
+                            <ul className="menu-list__subcategories">
+                                {category.items.map((item, itemIndex) => (
+                                    <li key={itemIndex} className="menu-list__subcategories__item">
+                                        <Link
+                                            to={`/products/${category.category.toLowerCase()}/${item.name.toLowerCase()}`}>{item.name}</Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navigation;
+
