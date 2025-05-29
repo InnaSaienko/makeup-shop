d7c830f95a89eca085afb2ae2d0366e21973d643
Inna Saienko <inna.saienko.nl@gmail.com>
2025-05-29 09:52:20 +0200

    turn all methods from Context to Redux slice

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = {
     users: [],
@@ -8,6 +8,34 @@ const initialState = {
 const authSlice = createSlice({
     name: 'auth',
     initialState,
-    reducers: {},
+    reducers: {
+        setUsers: (state, action) => {
+            state.users = action.payload;
+        },
+        setLoggedUser: (state, action) => {
+            state.loggedUser = action.payload;
+            state.isLoggedIn = action.payload !== 'guest@example.com';
+            localStorage.setItem('loggedUser', action.payload);
+        },
+        userSignUp: (state, action) => {
+            const newUser = action.payload;
+            state.users = [...state.users, newUser];
+            state.loggedUser = newUser.email;
+            state.isLoggedIn = true;
+            localStorage.setItem('loggedUser', newUser.email);
+        },
+        logIn: (state, action) => {
+            state.loggedUser = action.payload;
+            state.isLoggedIn = true;
+            localStorage.setItem('loggedUser', action.payload);
+        },
+        signOut: (state) => {
+            state.loggedUser = 'guest@example.com';
+            state.isLoggedIn = false;
+            localStorage.removeItem('loggedUser');
+        },
+    },
 });
+
+export const {setUsers, setLoggedUser, userSignUp, logIn, signOut} = authSlice.actions;
 export const authReducer = authSlice.reducer;
