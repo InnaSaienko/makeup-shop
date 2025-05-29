a57161438778915b457ab9bcfed3ffb54318ac13
Inna Saienko <inna.saienko.nl@gmail.com>
2025-05-29 10:27:56 +0200

    add selector selectLoggedUser

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,2 +1,4 @@
 export const selectUsers = state => state.auth.users;
 export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+
+export const selectLoggedUser = state => state.auth.loggedUser;
