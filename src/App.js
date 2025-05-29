d2cd19d2faa3a6d50ef3a29bfcf88398384a6368
Inna Saienko <inna.saienko.nl@gmail.com>
2025-05-29 16:19:02 +0200

    switch index.js/App to redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.scss";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
-import { BasketProvider } from "./context/BasketContext/BasketContext";
-import { AuthorizationProvider } from "./context/AuthorizationContext/AuthorizationContext";
 import Footer from "./layout/Footer/Footer";
 import RouterSet from "./components/Routes/RouterSet";
 import Layout from "./layout/Layout";
@@ -11,13 +9,9 @@ const App = () => {
   return (
       <div className="main-wrap">
         <ErrorBoundary>
-          <AuthorizationProvider>
-            <BasketProvider>
               <Layout>
                   <RouterSet />
               </Layout>
-            </BasketProvider>
-          </AuthorizationProvider>
           <Footer />
         </ErrorBoundary>
       </div>
