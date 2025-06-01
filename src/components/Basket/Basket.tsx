33fb9d06aedc0adadbe9f1f7ce1c13cc620b97b7
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-01 14:47:11 +0200

    reconnect Basket component from context to redux store

--- src/components/Basket/Basket.tsx
+++ src/components/Basket/Basket.tsx
@@ -1,25 +1,30 @@
 import React from "react";
-import { useBasket } from "../../context/BasketContext/BasketContext";
 import BasketCard from "../../components/BasketCard/BasketCard";
 import emptyBasket from "../../assets/images/empty-cart.svg";
 import "./Basket.scss";
+import {useDispatch, useSelector} from "react-redux";
+import {selectBasketItems} from "../../redux/selectors";
+import {closeBasket} from "../../redux/basketSlice";
+
+
 
 const Basket: React.FC<BasketProps> = ({ isOpen }) => {
-  const { closeBasket, basketProductsContext } = useBasket();
+  const dispatch = useDispatch();
+  const basketProducts = useSelector(selectBasketItems);
 
   if (!isOpen) {
     return null;
   }
 
-  const isEmpty = basketProductsContext.length === 0;
+  const isEmpty = basketProducts.length === 0;
 
   return (
     <>
-      <div className="background-overlay" onClick={closeBasket}></div>
+    <div className="background-overlay" onClick={() => dispatch(closeBasket())}></div>
       <div className="basket">
         <div className="basket-header">
           Shopping Cart
-          <span className="close" onClick={closeBasket}>
+          <span className="close" onClick={() => dispatch(closeBasket())}>
             &times;
           </span>
         </div>
@@ -30,7 +35,7 @@ const Basket: React.FC<BasketProps> = ({ isOpen }) => {
               <p>Your basket is empty</p>
             </div>
           ) : (
-              basketProductsContext.map((product) => (
+              basketProducts.map((product) => (
             <BasketCard
               key={`${product.id}}`}
               {...product}
