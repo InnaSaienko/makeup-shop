37faccce6d4fb9216b1839c103efd4d5ccf5ecaa
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-01 14:42:57 +0200

    add basket selectors

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -3,3 +3,13 @@ import {RootState} from "./store";
 export const selectUsers = (state: RootState) => state.auth.users;
 export const selectIsLoggedIn =(state: RootState)=> state.auth.isLoggedIn;
 export const selectLoggedUser = (state: RootState) => state.auth.loggedUser;
+export const selectBasketItems = (state: RootState) => state.basket.items;
+export const selectTotalQuantity = (state: RootState) =>
+    state.basket.items.reduce((total, item) => total + item.quantity, 0);
+export const selectProductQuantity = (id: string, color: string) => (state: RootState) => {
+    const item = state.basket.items.find(i => i.id === id && i.selectedColor === color);
+    return item?.quantity || 0;
+};
+
+
+
