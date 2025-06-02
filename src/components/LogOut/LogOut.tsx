5cc84aad3975503ec0ff8e18f30bbff18ed8f0db
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-02 10:36:45 +0200

    reconnect LogOut component from context to redux store

--- src/components/LogOut/LogOut.tsx
+++ src/components/LogOut/LogOut.tsx
@@ -1,19 +1,21 @@
 import React, {JSX} from 'react';
-// @ts-ignore
-import {useAuthorization} from "../../context/AuthorizationContext/AuthorizationContext";
+import {useDispatch, useSelector} from "react-redux";
+import {selectLoggedUser} from "../../redux/selectors";
+import {closeAuthorization, signOut} from "../../redux/authSlice";
 
 const LogOut = () : JSX.Element => {
-    const {loggedUser, closeAuthorization, signOut} = useAuthorization();
+    const dispatch = useDispatch();
+    const loggedUser = useSelector(selectLoggedUser);
 
     return (
         <>
-            <div className="background-overlay" onClick={closeAuthorization}></div>
+            <div className="background-overlay" onClick={() => dispatch(closeAuthorization())}></div>
             <div className="modal-auth">
                 <h2>Welcome, {loggedUser}!</h2>
                 <button
                     type="button"
                     className="button full-width"
-                    onClick={() => signOut(loggedUser)}
+                    onClick={() => dispatch(signOut())}
                 >
                     Log Out
                 </button>
