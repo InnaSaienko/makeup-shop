f1dbffac9cf38933154224b6527ffda78ca530ce
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-02 10:30:39 +0200

    define types of selectors params

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -5,9 +5,9 @@ export const selectIsLoggedIn = (state: RootState) => state.auth.isLoggedIn;
 export const selectLoggedUser = (state: RootState) => state.auth.loggedUser;
 export const selectBasketItems = (state: RootState) => state.basket.items;
 export const selectTotalQuantity = (state: RootState) =>
-    state.basket.items.reduce((total, item) => total + item.quantity, 0);
+    state.basket.items.reduce((total: number, item: BasketItem) => total + item.quantity, 0);
 export const selectProductQuantity = (id: string, color: string) => (state: RootState) => {
-    const item = state.basket.items.find(i => i.id === id && i.selectedColor === color);
+    const item : BasketItem | undefined = state.basket.items.find((item: { id: string; selectedColor: string; }) => item.id === id && item.selectedColor === color);
     return item?.quantity || 0;
 };
 
