2092b76d2f471c425e390d6f4a73b335bc7c6a2f
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-06 11:53:48 +0200

    User - reconnect from context to redux store

--- src/layout/HeaderTop/User.tsx
+++ src/layout/HeaderTop/User.tsx
@@ -1,12 +1,15 @@
 import React, {JSX} from 'react';
-import {useAuthorization} from "../../context/AuthorizationContext/AuthorizationContext";
+import {useDispatch, useSelector} from "react-redux";
+import {selectLoggedUser} from "../../redux/selectors";
+import {openAuthorization} from "../../redux/authSlice";
 
 const User = () : JSX.Element => {
-    const {openAuthorization, loggedUser, guest} = useAuthorization();
+    const dispatch = useDispatch();
+    const loggedUser = useSelector(selectLoggedUser);
 
     return (
-        <div className={`authorization ${loggedUser !== guest ? "logged-in" : "logged-out"}`}
-             onClick={openAuthorization}
+        <div className={`authorization ${loggedUser !== "guest" ? "logged-in" : "logged-out"}`}
+             onClick={() => dispatch(openAuthorization())}
         >
         </div>
     );
