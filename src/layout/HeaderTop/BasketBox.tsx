a7067463ab8f493e77db01ff0e79e1279c2fa090
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-12 16:23:54 +0200

    BasketBox - change logic for opening BasketBox

--- src/layout/HeaderTop/BasketBox.tsx
+++ src/layout/HeaderTop/BasketBox.tsx
@@ -1,19 +1,28 @@
 import React, {JSX} from 'react';
 import "./HeaderTop.scss";
 import {useDispatch, useSelector} from "react-redux";
-import {selectTotalQuantity} from "../../redux/selectors";
+import {selectIsBasketOpen, selectTotalQuantity} from "../../redux/selectors";
 import {openBasket} from "../../redux/basketSlice";
+import Basket from "../../components/Basket/Basket";
 
-const BasketBox  = (): JSX.Element => {
+const BasketBox = (): JSX.Element => {
     const dispatch = useDispatch();
     const productQuantity = useSelector(selectTotalQuantity);
+    const isBasketOpen = useSelector(selectIsBasketOpen);
+
+    const handleClick = () => {
+        dispatch(openBasket());
+    };
 
     return (
-        <div className="basket-box">
-            <button className={`basket-widget ${productQuantity === null ? "empty" : ""}`} onClick={() => dispatch(openBasket())}>
-                <span className={`header-counter${productQuantity === null ? "hidden" : ""}`}>{productQuantity}</span>
-            </button>
-        </div>
+        <>
+            <div className="basket-box">
+                <button className={`basket-widget ${productQuantity === null ? "empty" : ""}`} onClick={handleClick}>
+                    <span className={`header-counter${productQuantity === null ? "hidden" : ""}`}>{productQuantity}</span>
+                </button>
+            </div>
+            {isBasketOpen && <Basket/>}
+        </>
     );
 };
 
