a30fcca614d171d61ca3ab6e26af95dc0c155fd0
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-12 16:14:26 +0200

    LogIn - change logic for auth components

--- src/layout/HeaderTop/User.tsx
+++ src/layout/HeaderTop/User.tsx
@@ -1,17 +1,29 @@
 import React, {JSX} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {selectLoggedUser} from "../../redux/selectors";
+import {selectIsAuthOpen, selectIsLoggedIn} from "../../redux/selectors";
 import {openAuthorization} from "../../redux/authSlice";
+import LogIn from "../../components/LogIn/LogIn";
+import LogOut from "../../components/LogOut/LogOut";
 
-const User = () : JSX.Element => {
+const User = (): JSX.Element => {
     const dispatch = useDispatch();
-    const loggedUser = useSelector(selectLoggedUser);
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+    const isAuthOpen = useSelector(selectIsAuthOpen);
+
+    const handleClick = () => {
+        dispatch(openAuthorization());
+    };
 
     return (
-        <div className={`authorization ${loggedUser !== "guest" ? "logged-in" : "logged-out"}`}
-             onClick={() => dispatch(openAuthorization())}
-        >
-        </div>
+        <>
+            <div className={`authorization ${isLoggedIn ? "logged-in" : "logged-out"}`}
+                 onClick={handleClick}
+            ></div>
+            {isAuthOpen && (
+                isLoggedIn ? <LogOut /> : <LogIn />
+            )}
+        </>
+
     );
 };
 
