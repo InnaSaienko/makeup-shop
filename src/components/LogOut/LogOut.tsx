e2085ba9d8605fc8c38ae7757e290c3c5fc2635b
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-12 16:10:36 +0200

    LogOut - change logic for log out

--- src/components/LogOut/LogOut.tsx
+++ src/components/LogOut/LogOut.tsx
@@ -1,22 +1,26 @@
 import React, {JSX} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {selectLoggedUser} from "../../redux/selectors";
-import {closeAuthorization, signOut} from "../../redux/authSlice";
+import {closeAuthorization, logOut} from "../../redux/authSlice";
+import {selectUser} from "../../redux/selectors";
 
-const LogOut = () : JSX.Element => {
+const LogOut = (): JSX.Element => {
     const dispatch = useDispatch();
-    const loggedUser = useSelector(selectLoggedUser);
+    const user = useSelector(selectUser);
+
+    const handleLogout = () => {
+        dispatch(logOut()); // actual logout logic
+        dispatch(closeAuthorization()); // close the modal after logout
+    };
 
     return (
         <>
             <div className="background-overlay" onClick={() => dispatch(closeAuthorization())}></div>
             <div className="modal-auth">
-                <h2>Welcome, {loggedUser}!</h2>
+                <h2>Welcome, {user.email}!</h2>
                 <button
                     type="button"
                     className="button full-width"
-                    onClick={() => dispatch(signOut())}
-                >
+                    onClick={handleLogout}>
                     Log Out
                 </button>
             </div>
